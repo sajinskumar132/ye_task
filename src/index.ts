bd3fa96b095b30ce569cfb66b7f7e9faacb3b365
sajinskumar132 <sajinskumar132@gmail.com>
2025-01-22 20:51:48 +0530

    update api endpoint

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ config()
 const app= express()
 app.use(express.json())
 
-app.use(`/ye_task/${process.env.API_VERSION}/`,routes)
+app.use(`/api/${process.env.API_VERSION}/`,routes)
 
 DatabaseConnection.DbConnection(process.env.MONGODB_URI||null).then((response)=>{
     console.log(response)
